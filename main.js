fde2ecbbb1de4e1ff59736cf3fdd19dd7e7bbbce
Ru1n00 <ruhul.amin.tnt@gmail.com>
2024-10-10 07:43:23 +0600

    Initial agora setup

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// agora variables
+let appId = "38fdd9a342e44adcaf32ca3b7f4209b6"  // fake app ID
+let token = null;
+let uid = String(Math.floor(Math.random() * 1000000000));
+let client;
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -15,6 +22,18 @@ const servers = {
 }
 
 let init = async () => {
+    client = await AgoraRTM.createInstance(appId);
+    console.log("AgoraRTC client initialized", client);
+
+    await client.login({uid, token}).catch(console.error);
+    console.log("AgoraRTC client logged in", client);
+
+    channel = client.createChannel("main");
+    await channel.join().catch(console.error);
+
+    channel.on('MemberJoined', handleUserJoined);
+
+
     let devices = await navigator.mediaDevices.enumerateDevices();
     let audioInputDevices = devices.filter((device) => device.kind === "audioinput");
     let videoInputDevices = devices.filter((device) => device.kind === "videoinput");
@@ -42,7 +61,9 @@ let init = async () => {
     createOffer();
 };
 
-init();
+let handleUserJoined = async (memberId) => {
+    console.log("User joined:", memberId);
+}
 
 let createOffer = async () => {
     // create RTCPeerConnection
@@ -61,14 +82,14 @@ let createOffer = async () => {
         console.log("ontrack event:", event);
         event.streams[0].getTracks().forEach(track => remoteStream.addTrack(track));
     }
-
+    
     // add ICE candidate
     peerConnection.onicecandidate = (event) => {
         if(event.candidate) {
             console.log("ICE candidate:", event.candidate);
         }
     }
-
+    
 
 
     // create offer
@@ -77,9 +98,10 @@ let createOffer = async () => {
     console.log("Offer:", offer);
     // return offer;
 
-
+    
 
     console.log("peerConnection:", peerConnection);
 }
 
 // createOffer();
+init();
